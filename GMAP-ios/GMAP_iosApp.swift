d46408dbd0bdac199a3e6f0a97d705bb411d98a4
rvshah2217 <rvshah2217@gmail.com>
2023-09-15 10:43:43 +0530

    Initial Commit

---
+++ GMAP-ios/GMAP_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  GMAP_iosApp.swift
+//  GMAP-ios
+//
+//  Created by Ravi Shah on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct GMAP_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
