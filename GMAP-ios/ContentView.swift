bc19d8e1fe57a129ecd4c53251b760ba7c8cce60
rvshah2217 <rvshah2217@gmail.com>
2023-09-16 12:32:02 +0530

    Chore: Call ViewController Here

--- GMAP-ios/ContentView.swift
+++ GMAP-ios/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            //MARK: Call Viewcontroller File Here
             MapViewControllerWrapper().ignoresSafeArea(.all)
         }
     }
